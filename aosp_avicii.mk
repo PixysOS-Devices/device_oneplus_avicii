c160a209fd728207f73dd4b4f2f897c7b696dcfa
jhenrique09 <jhsv09@gmail.com>
2021-10-18 18:12:05 +0200

    avicii: Enable NGA

--- aosp_avicii.mk
+++ aosp_avicii.mk
@@ -27,6 +27,7 @@ $(call inherit-product, vendor/aosp/config/common_full_phone.mk)
 # PixelExperience Properties
 TARGET_USES_AOSP_RECOVERY := true
 TARGET_USES_CUSTOM_AVB_KEY := true
+TARGET_SUPPORTS_NEXT_GEN_ASSISTANT := true
 
 PRODUCT_NAME := aosp_avicii
 PRODUCT_DEVICE := avicii
